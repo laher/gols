726009801d174050fdb8464fa841957c6835ed59
Am Laher <amlaher@Ams-MacBook-Pro.local>
2016-03-25 19:58:03 +1300

    initial commit - very basic 'go ls' wrapper for ignoring paths containing 'vendor'

---
+++ gols.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	err := lsdirs(os.Args)
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func lsdirs(args []string) error {
+	cmd := exec.Command("go", "list", "./...")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	scanner := bufio.NewScanner(stdout)
+	go func() {
+		for scanner.Scan() {
+			txt := scanner.Text()
+			if !strings.Contains(txt, "vendor") {
+				fmt.Println(txt)
+			}
+		}
+	}()
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+	//log.Printf("Waiting for command to finish...")
+	err = cmd.Wait()
+	if err != nil {
+		log.Printf("Command finished with error: %v", err)
+		return err
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+	return err
+}
