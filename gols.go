125e37a4c6ba8e7612aac03c8d4deb4f3a820025
Am Laher <amlaher@Ams-MacBook-Pro.local>
2016-03-25 20:31:11 +1300

    basic support for running 'go test'

--- gols.go
+++ gols.go
@@ -2,50 +2,90 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
 )
 
 func main() {
 	err := lsdirs(os.Args)
 	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
 func lsdirs(args []string) error {
-	cmd := exec.Command("go", "list", "./...")
+	var ignoreDirsS = flag.String("ignore", "/vendor/", "ignore packages (comma-delimited)")
+	var execs = flag.String("exec", "", "exec (e.g. 'go test')")
+	flag.Parse()
+	cmd := exec.Command("go", "list")
+	args = flag.Args()
+	cmd.Args = append(cmd.Args, args...)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		return err
 	}
-
+	ignoreDirs := []string{}
+	if *ignoreDirsS != "" {
+		ignoreDirs = strings.Split(*ignoreDirsS, ",")
+	}
 	scanner := bufio.NewScanner(stdout)
+	lines := []string{}
+	l := sync.Mutex{}
 	go func() {
+		l.Lock()
 		for scanner.Scan() {
 			txt := scanner.Text()
-			if !strings.Contains(txt, "/vendor/") {
-				fmt.Println(txt)
+			ignore := false
+			for _, i := range ignoreDirs {
+				if strings.Contains(txt, i) {
+					ignore = true
+					break
+				}
+			}
+			if !ignore {
+				if *execs == "" {
+					fmt.Println(txt)
+				} else {
+					lines = append(lines)
+				}
 			}
 		}
+		l.Unlock()
 	}()
 	err = cmd.Start()
 	if err != nil {
 		return err
 	}
-	//log.Printf("Waiting for command to finish...")
 	err = cmd.Wait()
 	if err != nil {
-		log.Printf("Command finished with error: %v", err)
 		return err
 	}
-
 	err = scanner.Err()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		return err
+	}
+	l.Lock()
+	defer l.Unlock()
+	if *execs != "" {
+		//TODO quotes
+		execArr := strings.Split(*execs, " ")
+		cmd2 := exec.Command(execArr[0])
+		cmd2.Args = execArr
+		cmd2.Stdout = os.Stdout
+		cmd2.Stderr = os.Stderr
+		err = cmd2.Start()
+		if err != nil {
+			return err
+		}
+		err = cmd2.Wait()
+		if err != nil {
+			return err
+		}
 	}
 	return err
 }
